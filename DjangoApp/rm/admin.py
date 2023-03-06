cd4aeed2e634b4f70c860ed8d37bd3696e2dc66f
JunoTsangChunLung <junotsang1029@gmail.com>
2023-03-07 01:22:50 +0800

    admin.py

--- DjangoApp/rm/admin.py
+++ DjangoApp/rm/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Category, Subcategory
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    pass
